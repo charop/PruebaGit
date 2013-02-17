1de71b0414bbcdaf0656500eee734173ccef957e
charop <mrprado@iescomercio.com>
2013-02-17 13:07:31 +0100

    Update src/mipruebagit/PruebaGit.java

--- src/mipruebagit/PruebaGit.java
+++ src/mipruebagit/PruebaGit.java
@@ -8,7 +8,7 @@ public class PruebaGit {
 	 * @param args El programa no trata los argumentos.
 	 */
 	public static void main(String[] args) {
-		System.out.println("Subversion, tu antes molabas");
+		System.out.println("Subversion, tu antes molabas, pero ahora Git mola más");
 
 	}
 
