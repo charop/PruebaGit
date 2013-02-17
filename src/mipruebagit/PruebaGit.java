90102f67f638dd6f1d4d6218c19362dcb0b37ef9
Charo <mrprado@iescomercio.com>
2013-02-17 12:19:01 +0100

    Commit inicial del proyecto.

---
+++ src/mipruebagit/PruebaGit.java
@@ -0,0 +1,15 @@
+package mipruebagit;
+
+public class PruebaGit {
+
+	/**
+	 * Clase para ejemplo de uso de EGit.
+	 * @author Prado
+	 * @param args El programa no trata los argumentos.
+	 */
+	public static void main(String[] args) {
+		System.out.println("A ver si Git nos gusta");
+
+	}
+
+}
