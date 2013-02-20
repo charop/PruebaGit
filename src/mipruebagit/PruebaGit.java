413dfd783941f05db82b283c621127a31af56ecf
Charo <mrprado@iescomercio.com>
2013-02-20 15:28:36 +0100

    No hay manera

--- src/mipruebagit/PruebaGit.java
+++ src/mipruebagit/PruebaGit.java
@@ -6,6 +6,7 @@ public class PruebaGit {
 	 * Clase para ejemplo de uso de EGit.
 	 * @author Prado
 	 * @param args El programa no trata los argumentos.
+	 * No hay manera
 	 */
 	public static void main(String[] args) {
 		System.out.println("Lo modifico en local y lo quiero subir a github");
